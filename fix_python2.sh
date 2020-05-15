ed1c7d6cb712e78dcef3cc85363c4892cc6e26b4
desertsniper87 <torshobuet@gmail.com>
2020-05-16 02:41:02 +0600

    initial commit

---
+++ fix_python2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author            : desertsniper87 <torshobuet@gmail.com>
+# Date              : 16.05.2020
+# Last Modified Date: 16.05.2020
+
+python2 -m ipykernel install --user
+sudo apt install -y python2,7-dev
+
